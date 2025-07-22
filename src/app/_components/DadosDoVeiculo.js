35e1db2c3460330736ce0550af928d0d2adaeb1c
Nathan Campolina <nathancampolina.r@gmail.com>
2025-07-22 16:49:46 -0300

    create DadosDoVeiculo component

---
+++ src/app/_components/DadosDoVeiculo.js
@@ -0,0 +1,45 @@
+import { Stack, TextField } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers";
+import { useState } from "react";
+
+const DadosVeiculo = ({ atpvData, updateAtpvData }) => {
+    const [placa, setPlaca] = useState(atpvData.current.placa ?? "");
+    const [renavam, setRenavam] = useState(atpvData.current.renavam ?? "");
+    const [chassi, setChassi] = useState(atpvData.current.chassi ?? "");
+    const [crv, setCrv] = useState(atpvData.current.crv ?? "");
+    const [dataEmissaoCrv, setDataEmissaoCrv] = useState(atpvData.current.emissaoCrv ?? null);
+    const [numeroViaCrv, setNumeroViaCrv] = useState(atpvData.current.numeroViaCrv ?? "");
+    const [codigoSegurancaCrv, setCodigoSegurancaCrv] = useState(atpvData.current.codigoSegurancaCrv ?? "");
+    const [anoFabricacao, setAnoFabricacao] = useState(atpvData.current.anoFabricacao ?? "");
+    const [anoModelo, setAnoModelo] = useState(atpvData.current.anoModelo ?? "");
+    const [quilometragem, setQuilometragem] = useState(atpvData.current.quilometragem ?? "");
+
+    updateAtpvData({
+        placa: placa,
+        renavam: renavam,
+        chassi: chassi,
+        crv: crv,
+        dataEmissaoCrv: dataEmissaoCrv,
+        numeroViaCrv: numeroViaCrv,
+        codigoSegurancaCrv: codigoSegurancaCrv,
+        anoFabricacao: anoFabricacao,
+        anoModelo: anoModelo,
+        quilometragem: quilometragem,
+    })
+    console.log(atpvData.current)
+
+    return <Stack spacing={2}>
+        <TextField value={placa} onChange={e => {setPlaca(e.target.value)}} label="Placa" size="small"/>
+        <TextField value={renavam} onChange={e => {setRenavam(e.target.value)}} label="Renavam" size="small"/>
+        <TextField value={chassi} onChange={e => {setChassi(e.target.value)}} label="Chassi" size="small"/>
+        <TextField value={crv} onChange={e => {setCrv(e.target.value)}} label="CRV" size="small"/>
+        <DatePicker label="Data Emissao CRV" />
+        <TextField value={numeroViaCrv} onChange={e => {setNumeroViaCrv(e.target.value)}} label="Numero da Via CRV" size="small"/>
+        <TextField value={codigoSegurancaCrv} onChange={e => {setCodigoSegurancaCrv(e.target.value)}} label="Codigo de Seguranca CRV" size="small"/>
+        <TextField value={anoFabricacao} onChange={e => {setAnoFabricacao(e.target.value)}} label="Ano Fabricacao" size="small"/>
+        <TextField value={anoModelo} onChange={e => {setAnoModelo(e.target.value)}} label="Ano Modelo" size="small"/>
+        <TextField value={quilometragem} onChange={e => {setQuilometragem(e.target.value)}} label="Quilometragem" size="small"/>
+    </Stack>
+}
+
+export default DadosVeiculo;
